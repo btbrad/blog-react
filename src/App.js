0259dfd473257d416e013d60c88f568e5f467f80
bt <biantaobrad@gmail.com>
2020-08-13 09:42:47 +0800

    配置路由

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import { GlobalStyle } from './style.js'
-import Header from './common/header/index'
 import { Provider } from 'react-redux'
 import store from './store'
+import { BrowserRouter, Route } from 'react-router-dom'
+import Header from './common/header/index'
+import Home from './pages/home'
+import Detail from './pages/detail'
 
 function App() {
   return (
@@ -10,6 +13,10 @@ function App() {
       <div className='app'>
         <GlobalStyle />
         <Header />
+        <BrowserRouter>
+          <Route path="/" exact component={ Home }></Route>
+          <Route path="/detail" exact component={ Detail }></Route>
+        </BrowserRouter>
       </div>
     </Provider>
   )
