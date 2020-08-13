5c253926b9476e861c354cf86461d85d786be8d2
bt <biantaobrad@gmail.com>
2020-08-13 11:17:45 +0800

    首页文章列表完成

--- src/pages/home/components/ArticleItem/index.js
+++ src/pages/home/components/ArticleItem/index.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import { ArticleItem, ArticleTitle, ArticleContent, ArticleBottom } from '../../style'
+import Icon from '../../../../components/SvgIcon'
 
-export default props => (
+export default ({ data }) => (
   <ArticleItem>
-    <ArticleTitle></ArticleTitle>
-    <ArticleContent></ArticleContent>
-    <ArticleBottom></ArticleBottom>
+    <img className='article-img' src={data.imgUrl} alt="" />
+    <ArticleTitle>{data.title}</ArticleTitle>
+    <ArticleContent>{data.content}</ArticleContent>
+    <ArticleBottom>
+      <span>
+        <Icon name="star" />
+        {data.star}
+      </span>
+      <span>
+        <Icon name="comment" />
+        {data.comment}
+      </span>
+      <span>
+        <Icon name="like" />
+        {data.like}
+      </span>
+    </ArticleBottom>
   </ArticleItem>
 )
--- src/pages/home/style.js
+++ src/pages/home/style.js
@@ -18,6 +18,7 @@ export const HomeRight = styled.div`
 export const ArticleList = styled.div`
   width: 100%;
   padding-left: 20px;
+  margin-top: 50px;
   height: auto;
 `
 
@@ -25,23 +26,35 @@ export const ArticleItem = styled.div`
   width: 100%;
   height: 200px;
   border-bottom: 1px solid #ccc;
+  padding-top: 20px;
+  .article-img {
+    width: 148px;
+    height: 98px;
+    float: right;
+  }
 `
 
 export const ArticleTitle = styled.p`
   height: 50px;
-  width: 100%;
-  font-size: 30px;
+  width: 70%;
+  font-size: 22px;
   font-weight: 900;
   word-break: break-word;
+  margin-bottom: 10px;
+  cursor: pointer;
 `
 
 export const ArticleContent = styled.div`
-  width: 100%;
+  width: 70%;
   height: 100px;
   line-height: 20px;
+  color: #999;
 `
 
 export const ArticleBottom = styled.div`
-  width: 100%;
+  width: 70%;
   height: 50px;
+  color: #999;
+  display: flex;
+  justify-content: space-around;
 `
